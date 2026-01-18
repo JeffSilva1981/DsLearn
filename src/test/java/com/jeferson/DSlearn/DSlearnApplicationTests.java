013f2a96ca28a11f68f72f794e00870eb464d1e5
JeffSilva1981 <jeff.contactwork@gmail.com>
2026-01-18 17:17:31 -0300

    User, Role

---
+++ src/test/java/com/jeferson/DSlearn/DSlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeferson.DSlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
