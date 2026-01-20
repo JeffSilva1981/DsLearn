cba98dd5288c305af153a036696347d82aa19c82
JeffSilva1981 <jeff.contactwork@gmail.com>
2026-01-19 21:04:21 -0300

    Course

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,3 +13,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('BootCamp Java', 'https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/java.png', 'https://freesvg.org/img/1643312660java.png');
